0a7f266cd2c0b2f40b7e3d6a6c41c1b39ba498d2
emkeator <emkeator@gmail.com>
2018-01-23 18:36:49 -0700

    tested with new data, built CRUD for new data

--- data.js
+++ data.js
@@ -1,26 +1,49 @@
-module.exports = [
-    {
-        id: 1,
-        title: 'The Complete Node.js Developer Course',
-        author: 'Andrew Mead, Rob Percival',
-        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs/'
-    },
-    {
-        id: 2,
-        title: 'Node.js, Express & MongoDB Dev to Deployment',
-        author: 'Brad Traversy',
-        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
-    },
-    {
-        id: 3,
-        title: 'JavaScript: Understanding The Weird Parts',
-        author: 'Anthony Alicea',
-        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
-        topic: 'JavaScript',
-        url: 'https://codingthesmartway.com/courses/understand-javascript/'
-    }
-]
+// module.exports = [
+//     {
+//         id: 1,
+//         title: 'The Complete Node.js Developer Course',
+//         author: 'Andrew Mead, Rob Percival',
+//         description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+//         topic: 'Node.js',
+//         url: 'https://codingthesmartway.com/courses/nodejs/'
+//     },
+//     {
+//         id: 2,
+//         title: 'Node.js, Express & MongoDB Dev to Deployment',
+//         author: 'Brad Traversy',
+//         description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+//         topic: 'Node.js',
+//         url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
+//     },
+//     {
+//         id: 3,
+//         title: 'JavaScript: Understanding The Weird Parts',
+//         author: 'Anthony Alicea',
+//         description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+//         topic: 'JavaScript',
+//         url: 'https://codingthesmartway.com/courses/understand-javascript/'
+//     }
+// ]
+
+
+module.exports = [{
+    name: 'Diana',
+    title: 'of Themiscyra',
+    powers: 'Super strength, super speed, fighting, flighting, lightning, concussive blasts',
+    description: 'Molded from clay, amazing Amazon goddess warrior',
+    awesomeness: 100
+},
+{
+    name: 'Kara',
+    title:'of Krypton',
+    powers: 'Super strength, super speed, flighting, laser vision, x-ray vision, ice breath',
+    description: 'Kryptonian sent to earth granted powers by yellow sun radiation',
+    awesomeness: 99
+},
+{
+    name: 'Leia',
+    title:'Princess of Alderaan',    
+    powers: 'Excellent shot, diplomacy, rebellion, espionage, tactics & command, the Force',
+    description: 'Force-wielding rebel daughter of Darth Vader and Padme Amidala',
+    awesomeness: 99
+}]
--- server.js
+++ server.js
@@ -1,19 +1,83 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
+var ww = require('./data');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  input wonderWomanInput {
+    name: String
+    title: String
+    powers: String
+    description: String
+    awesomeness: Int
+  }
+  type wonderWoman {
+    name: String
+    title: String
+    powers: String
+    description: String
+    awesomeness: Int
+  }
+  type Mutation {
+    createWonderWoman(input: wonderWomanInput): wonderWoman
+    updateWonderWomanDesc(name: String!, description: String!): wonderWoman
+    deleteWonderWoman(name: String!): wonderWoman
+  }
   type Query {
-    hello: String
+    hello: String    
+    wonderwoman(name: String): wonderWoman
+    wonderwomen: [wonderWoman]
   }
 `);
+
+class wonderWoman {
+    constructor({name, title, powers, description, awesomeness}) {
+      this.name = name;
+      this.title = title;
+      this.powers = powers;
+      this.description = description;
+      this.awesomeness = awesomeness;
+    }
+  }
+
+function getWW(name){
+  console.log(name)
+  return ww.filter(e => e.name === name.name)[0]
+}
+
+function getWWPlural(){
+  return ww
+}
+
+function newWW({input}){
+  ww.push(new wonderWoman(input))
+  return new wonderWoman(input)
+}
+
+function updateWW(args){
+  let {name, description} = args
+  let index = ww.findIndex(e => e.name === name)
+  ww[index].description = description
+  return ww[index]
+}
+
+function deleteWW(args){
+  let {name} = args
+  let index = ww.findIndex(e => e.name === name)
+  return ww.splice(index, 1)
+}
 
 // The root provides a resolver function for each API endpoint
 var root = {
   hello: () => {
-    return 'Hello world!';
+    return "Want to hear about some awesome ladies? You can search for 'Diana', 'Kara', 'Leia', 'Hermione', and 'Galadriel'.";
   },
+  wonderwoman: getWW,
+  wonderwomen: getWWPlural,
+  createWonderWoman: newWW,
+  updateWonderWomanDesc: updateWW,
+  deleteWonderWoman: deleteWW
 };
 
 var app = express();
